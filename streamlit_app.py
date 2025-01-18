4c1a357cbe0757624aa6d1fa330efa661f00812d
LohTC <148647566+LohTC@users.noreply.github.com>
2025-01-18 17:23:38 +0800

    Update streamlit_app.py
    Get the SmoothieFroot Data to Show Nutrition Data for the Fruits Chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@
 # - first app: for customers to enter their orders
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session #del
 from snowflake.snowpark.functions import col
 
@@ -52,8 +53,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
-    #Create a Place to Store Order Data
+    #Create a Place to Store Order Data    
     my_insert_stmt = """ insert into smoothies.public.orders
         (ingredients, name_on_order)
         values ('""" + ingredients_string + """','""" + name_on_order + \
@@ -66,10 +69,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered! ' + name_on_order, icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
